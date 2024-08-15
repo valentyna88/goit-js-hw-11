c88d7fac326ddc3d3698e961ff080418abd90977
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-08-15 23:07:20 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,1 +1,18 @@
+export function fetchImages(query) {
+  const API_KEY = '45452240-d9bf2a206a145f9e2645b735d';
+  const API_URL = 'https://pixabay.com/api/';
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
 
+  return fetch(`${API_URL}?${params}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
