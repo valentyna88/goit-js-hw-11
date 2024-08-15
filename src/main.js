b06c062e0f52fd86dd02820a924c94fd4deb3540
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-08-15 16:09:32 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,2 +1,24 @@
-import * as pixabayApi from './js/pixabay-api';
-import * as renderFunctions from './js/render-functions';
+// import * as pixabayApi from './js/pixabay-api';
+// import * as renderFunctions from './js/render-functions';
+
+// https://pixabay.com/api
+
+/*
+<li class="gallery-card">
+  <img class="gallery-img" src="" alt="" />
+</li>
+*/
+
+// fetch(
+//   'https://pixabay.com/api/?key=45452240-d9bf2a206a145f9e2645b735d&q=dog&orientation=horizontal'
+// );
+
+const searchFormEl = document.querySelector('.js-search-form');
+
+const onSearchFormSubmit = event => {
+  event.preventDefault();
+
+  console.dir(searchFormEl);
+};
+
+searchFormEl.addEventListener('submit', onSearchFormSubmit);
