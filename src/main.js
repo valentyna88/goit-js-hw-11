dd6aa17c9ff4fb7af0175218f542cc08b37cd1b6
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-08-15 19:46:18 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // import * as pixabayApi from './js/pixabay-api';
 // import * as renderFunctions from './js/render-functions';
 
-// https://pixabay.com/api
-
-/*
-<li class="gallery-card">
-  <img class="gallery-img" src="" alt="" />
-</li>
-*/
-
-// fetch(
-//   'https://pixabay.com/api/?key=45452240-d9bf2a206a145f9e2645b735d&q=dog&orientation=horizontal'
-// );
-
 const searchFormEl = document.querySelector('.js-search-form');
+const galleryEl = document.querySelector('.js-gallery');
+
+const createGalleryCardTemplate = () => {
+  return `
+  <li class="gallery-card">
+    <img class="gallery-img" src="" alt="" />
+  </li>
+  `;
+};
 
 const onSearchFormSubmit = event => {
   event.preventDefault();
